b1c89297778624c54c66b542d6d940552550e637
Muhamad Fahry Pratama Putra <m.fahry.pratama.putra@gmail.com>
2026-09-07 21:27:14 +0700

    fix: update learning path content and add new SVG icons for presentation and search

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -121,7 +121,7 @@ export default function AboutPage() {
             <h1 className="text-center font-semibold text-base lg:text-xl">LEARNING PATH</h1>
             <hr className="border-light_blue border-1 w-16"/>
           </div>
-          <div className="flex flex-col lg:flex-row gap-2 md:gap-3 lg:gap-5 justify-center">
+          <div className="grid grid-cols-3 gap-2 md:gap-3 lg:gap-5 justify-items-center">
             <div className="flex lg:flex-col gap-6 lg:gap-8 items-center p-6 lg:px-10 lg:py-20 border-[1px] rounded-xl lg:rounded-[32px] w-full h-[105px] lg:w-[400px] lg:h-[496px] bg-neutral-50 border-gray-200">
               <div className="p-5 rounded-full bg-gradient-to-r from-gray-200 to-white">
                 <Image
@@ -155,7 +155,7 @@ export default function AboutPage() {
             <div className="flex lg:flex-col gap-6 lg:gap-8 items-center p-6 lg:px-10 lg:py-20 border-[1px] rounded-xl lg:rounded-[32px] w-full h-[105px] lg:w-[400px] lg:h-[496px] bg-neutral-50 border-gray-200">
               <div className="p-5 rounded-full bg-gradient-to-r from-gray-200 to-white">
                 <Image
-                  src={withBasePath("/images/puzzle-piece.svg")}
+                  src={withBasePath("/images/search.svg")}
                   alt="-"
                   className="justify-center lg:w-12 lg:h-12"
                   width={24}
@@ -164,7 +164,39 @@ export default function AboutPage() {
               </div>
               <div className="flex flex-col gap-1 lg:gap-4 items-start lg:items-center justify-start lg:justify-center text-left lg:text-center">
                 <h1 className="font-medium text-xl lg:text-4xl text-slate-700">DAY 2</h1>
-                <p className="font-medium text-sm lg:text-2xl text-gray-600">Solving the Mystery by Making Predictions with Data</p>
+                <p className="font-medium text-sm lg:text-2xl text-gray-600">Preparing the Evidence for Investigation</p>
+              </div>
+            </div>
+          </div>
+          <div className="flex gap-2 md:gap-3 lg:gap-5 justify-center">
+            <div className="flex lg:flex-col gap-6 lg:gap-8 items-center p-6 lg:px-10 lg:py-20 border-[1px] rounded-xl lg:rounded-[32px] w-full h-[105px] lg:w-[400px] lg:h-[496px] bg-neutral-50 border-gray-200">
+              <div className="p-5 rounded-full bg-gradient-to-r from-gray-200 to-white">
+                <Image
+                  src={withBasePath("/images/puzzle-piece.svg")}
+                  alt="-"
+                  className="justify-center lg:w-12 lg:h-12"
+                  width={24}
+                  height={24}
+                />
+              </div>
+              <div className="flex flex-col gap-1 lg:gap-4 items-start lg:items-center justify-start lg:justify-center text-left lg:text-center">
+                <h1 className="font-medium text-xl lg:text-4xl text-slate-700">DAY 3</h1>
+                <p className="font-medium text-sm lg:text-2xl text-gray-600">Solving the Mystery Through Predictive Modeling</p>
+              </div>
+            </div>
+            <div className="flex lg:flex-col gap-6 lg:gap-8 items-center p-6 lg:px-10 lg:py-20 border-[1px] rounded-xl lg:rounded-[32px] w-full h-[105px] lg:w-[400px] lg:h-[496px] bg-neutral-50 border-gray-200">
+              <div className="p-5 rounded-full bg-gradient-to-r from-gray-200 to-white">
+                <Image
+                  src={withBasePath("/images/presentation.svg")}
+                  alt="-"
+                  className="justify-center lg:w-12 lg:h-12"
+                  width={24}
+                  height={24}
+                />
+              </div>
+              <div className="flex flex-col gap-1 lg:gap-4 items-start lg:items-center justify-start lg:justify-center text-left lg:text-center">
+                <h1 className="font-medium text-xl lg:text-4xl text-slate-700">DAY 4</h1>
+                <p className="font-medium text-sm lg:text-2xl text-gray-600">Revealing the Answer: The Final Presentation</p>
               </div>
             </div>
           </div>
