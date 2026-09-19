426fb7e7dcca864e5d8f96a09a1b2e3e78617f27
Ablebil <hafizhfaiq07@gmail.com>
2026-09-19 17:10:02 +0700

    fix: remove dsmbc basePath

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactCompiler: true,
   output: "standalone",
-  basePath: "/dsmbc",
-  assetPrefix: "/dsmbc/",
 };
 
 export default nextConfig;
