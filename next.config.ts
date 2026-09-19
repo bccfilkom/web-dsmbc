c024b99cf6ea7cdc86200dcc004f33e128057e5e
Ablebil <hafizhfaiq07@gmail.com>
2026-09-19 16:53:10 +0700

    fix: add dsmbc basePath

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactCompiler: true,
   output: "standalone",
+  basePath: "/dsmbc",
+  assetPrefix: "/dsmbc/",
 };
 
 export default nextConfig;
