f9eb54dfec605dd6d650ee695ad49e66f7fbb7a9
Muhamad Fahry Pratama Putra <m.fahry.pratama.putra@gmail.com>
2026-09-20 09:04:59 +0700

    fix: update REGISTRATION_URL to new link

--- src/shared/constants/links.ts
+++ src/shared/constants/links.ts
@@ -1,3 +1,3 @@
-export const REGISTRATION_URL = "https://forms.gle/7MeYKZoor7C5ZMEV8";
+export const REGISTRATION_URL = "https://forms.gle/KBxENxLNagDjVvHi7";
 export const GUIDEBOOK_URL =
   "https://bccfilkom.ub.ac.id/dsmbc/files/Guidebook-DSMBC-Tata-Tertib-2026.pdf";
